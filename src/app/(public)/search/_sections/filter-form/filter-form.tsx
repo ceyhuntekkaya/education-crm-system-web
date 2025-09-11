d7465b9795a4f030bd159b3f264f7063ba60ae70
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2025-09-11 12:18:55 +0300

    `Added scrollbar styles and overflow utilities`

--- src/app/(public)/search/_sections/filter-form/filter-form.tsx
+++ src/app/(public)/search/_sections/filter-form/filter-form.tsx
@@ -155,7 +155,11 @@ const FormContent = () => {
                   forceOpen={(section as any).forceOpen || false}
                   className="mb-0"
                   titleClassName={titleClassName}
-                  contentClassName=""
+                  contentClassName={
+                    (section as any).forceOpen
+                      ? ""
+                      : "max-h-300 overflow-y-auto scroll-thin"
+                  }
                 >
                   {section.component}
                 </AccordionItem>
