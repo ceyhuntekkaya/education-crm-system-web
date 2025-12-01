fb54c75f002a278fb690d4b0761b3d4d5e4c2752
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2025-12-01 09:49:21 +0300

    feat: update EducationLanguages component to display language type label correctly

--- src/app/(public)/search/[id]/_shared/sections/institution-sidebar/components/education-languages.tsx
+++ src/app/(public)/search/[id]/_shared/sections/institution-sidebar/components/education-languages.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useInstitutionSidebarData } from "../hooks/useInstitutionSidebarData";
+import { getLanguageTypeLabel } from "@/utils";
 
 export const EducationLanguages: React.FC = () => {
   const { school } = useInstitutionSidebarData();
 
   return (
-    <div className="text-center mb-20">
+    <div className="text-center mb-32">
       <div className="text-neutral-500 text-sm mb-8">
-        <span className="fw-medium text-main-600">Ana Dil:</span>{" "}
-        {school.languageOfInstruction}
+        <span className="fw-medium text-main-600">Eğitim Dili:</span>{" "}
+        {school.languageOfInstruction
+          ? getLanguageTypeLabel(school.languageOfInstruction)
+          : "-"}
       </div>
       {school.foreignLanguages && (
         <div className="text-neutral-500 text-sm">
