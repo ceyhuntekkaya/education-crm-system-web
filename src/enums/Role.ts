65f0a3d1ee307bce8e4fe18372adc34399030daf
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2025-12-22 10:14:44 +0300

    feat: Extend Role and UserRoleDto enums to include new roles and permissions

--- src/enums/Role.ts
+++ src/enums/Role.ts
@@ -4,12 +4,18 @@ export enum Role {
   USER = "USER",
   ADMIN = "ADMIN",
   CANDIDATE = "CANDIDATE",
-  COMPANY = "COMPANY"
+  COMPANY = "COMPANY",
+  SUPPLY = "SUPPLY",
+  INSTRUCTOR = "INSTRUCTOR",
+  PARTICIPANT = "PARTICIPANT",
 }
 
 export const RolePermissions: Record<Role, Permission[]> = {
   USER: [Permission.USER_CREATE, Permission.SETTING],
   ADMIN: [Permission.GENERAL],
   CANDIDATE: [Permission.GENERAL],
-  COMPANY: [Permission.GENERAL]
+  COMPANY: [Permission.GENERAL],
+  SUPPLY: [Permission.GENERAL],
+  INSTRUCTOR: [Permission.GENERAL],
+  PARTICIPANT: [Permission.GENERAL],
 };
--- src/types/dto/user/UserRoleDto.ts
+++ src/types/dto/user/UserRoleDto.ts
@@ -1,17 +1,47 @@
 import { SchoolDto } from "../institution";
 
 export interface UserRoleDto {
-    /** Format: int64 */
-    id?: number;
-    /** Format: int64 */
-    userId?: number;
-    /** @enum {string} */
-    role?: "USER" | "ADMIN" | "CANDIDATE" | "COMPANY";
-    departments?: ("AUTHOR" | "GRADER" | "SUPERVISOR" | "MANAGEMENT" | "IT" | "AUTHOR_REVIEWER" | "ADMIN" | "REVIEWER")[];
-    permissions?: ("APPROVAL" | "USER_CREATE" | "GENERAL" | "FINANCE_OPERATION" | "ACCOUNTING_OPERATION" | "DELIVERY_OPERATION" | "CUSTOMER_OPERATION" | "OFFER_OPERATION" | "ORDER_OPERATION" | "SUPPLIER_OPERATION" | "TRANSPORTATION_OPERATION" | "DELIVERY_DOCUMENT" | "SETTING")[];
-    /** @enum {string} */
-    roleLevel?: "BRAND" | "CAMPUS" | "SCHOOL" | "SYSTEM";
-    /** Format: date-time */
-    expiresAt?: string;
-    schools?: SchoolDto[];
-};
+  /** Format: int64 */
+  id?: number;
+  /** Format: int64 */
+  userId?: number;
+  /** @enum {string} */
+  role?:
+    | "USER"
+    | "ADMIN"
+    | "CANDIDATE"
+    | "COMPANY"
+    | "SUPPLY"
+    | "INSTRUCTOR"
+    | "PARTICIPANT";
+  departments?: (
+    | "AUTHOR"
+    | "GRADER"
+    | "SUPERVISOR"
+    | "MANAGEMENT"
+    | "IT"
+    | "AUTHOR_REVIEWER"
+    | "ADMIN"
+    | "REVIEWER"
+  )[];
+  permissions?: (
+    | "APPROVAL"
+    | "USER_CREATE"
+    | "GENERAL"
+    | "FINANCE_OPERATION"
+    | "ACCOUNTING_OPERATION"
+    | "DELIVERY_OPERATION"
+    | "CUSTOMER_OPERATION"
+    | "OFFER_OPERATION"
+    | "ORDER_OPERATION"
+    | "SUPPLIER_OPERATION"
+    | "TRANSPORTATION_OPERATION"
+    | "DELIVERY_DOCUMENT"
+    | "SETTING"
+  )[];
+  /** @enum {string} */
+  roleLevel?: "BRAND" | "CAMPUS" | "SCHOOL" | "SYSTEM";
+  /** Format: date-time */
+  expiresAt?: string;
+  schools?: SchoolDto[];
+}
